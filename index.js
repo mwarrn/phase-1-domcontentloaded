fcb8656882414f7f47d713bfcb3baefd06582feb
Michael Warren <mwarrn@amazon.com>
2022-05-07 23:36:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const id = document.getElementById("text");
+    id.textContent = "This is really cool!";
+});
